dcd09cec14dd80d2ae1b4b9f30e1aa084b8b4070
Alba Rico <amiloyrico.36@gmail.com>
2022-05-17 18:52:39 -0500

    Initial Commit

---
+++ AimForThat 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AimForThat 2022
+//
+//  Created by Alba Yolima Rico Montenegro on 17/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
